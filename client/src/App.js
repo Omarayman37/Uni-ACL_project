06e2f039c26782a1bd9d1bf2e294a0f3ae28b506
karim lotfy <karim.samir.lotfy@gmail.com>
2021-11-26 17:40:19 +0100

    Added routing React

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import Guest from './components/Guest.js'
 import Airplanes from './components/Airplanes.js';
 import LoginPage from './components/Login';
-import {Router, Link, Route} from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div>
-      <header>
-        {/* <Airplanes /> */}
-        <LoginPage />
-      </header>
+      <header></header>
+      <Routes>
+        <Route path="/" element={<LoginPage />} exact />
+        <Route path="/LoginUser" element={<LoginPage />} />
+        <Route path="/RegisterUser" element={<Guest />} />
+        <Route path="/AddAirplanes" element={<Airplanes />} />
+      </Routes>
+      {/* <Airplanes /> */}
+      <LoginPage />
     </div>
   );
 }
