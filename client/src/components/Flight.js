b2b50c10c9631eccae84cb0967bc636ccb768427
karim lotfy <karim.samir.lotfy@gmail.com>
2021-12-22 20:19:12 +0100

    Merged manual with flight Omar aiman added expanding bar

--- client/src/components/Flight.js
+++ client/src/components/Flight.js
@@ -7,7 +7,7 @@ import {
   HeartFilled,
   EllipsisOutlined,
 } from "@ant-design/icons";
-import { Button, Card } from "antd";
+import { Button, Card, Menu } from "antd";
 import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
 const Flight = ({ flight }) => {
@@ -30,12 +30,18 @@ const Flight = ({ flight }) => {
         <p>Departure Time: {flight.departure_time} </p>
         <p>Arrival Time: {flight.arrival_time} </p>
         <p>Trip Duration: {flight.duration} </p>
-        <p>Cabin Class: {flight.cabin_class} </p>
-        <p>Baggage Allowance: {flight.baggage_allowance} </p>
         <p>Price: {flight.price} </p>
-        <p>Seats Left: {flight.SeatsLeft}</p>
         <p>from: {flight.from}</p>
         <p>to: {flight.to}</p>
+        <Menu mode="inline" style={{ height: "100%", width: "100%" }}>
+          <Menu.SubMenu title={"details"} key={"details"}>
+            <p style={{ height: 20 }}>Trip Duration: {flight.cabin_class} </p>
+            <p style={{ height: 20 }}>
+              Baggage Allowance: {flight.baggage_allowance}
+              <p>Seats Left: {flight.SeatsLeft}</p>
+            </p>
+          </Menu.SubMenu>
+        </Menu>
       </Card>
     </div>
   );
