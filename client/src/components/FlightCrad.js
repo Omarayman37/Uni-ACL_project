fffe7cbc55e9ad1fbe6165e4117758a854985235
Omarayman37 <omar.hasaballah@yahoo.com>
2021-12-22 01:09:09 +0100

    expand

--- client/src/components/FlightCrad.js
+++ client/src/components/FlightCrad.js
@@ -1,22 +1,33 @@
 import React, { useState }  from 'react';
 import ReactDOM, { render } from 'react-dom';
 import { Card } from 'antd';
-import { Typography  } from 'antd';
-import {HeartOutlined , EllipsisOutlined } from '@ant-design/icons';
+import { Typography , Menu , Layout } from 'antd';
+import {HeartOutlined , EllipsisOutlined   } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
 
 class FlightCard extends React.Component {   
+    
         render() {
         return(
             <div>
-            <Card title= {'Flight Number: ' + this.props.flight_id} extra={<a><HeartOutlined /></a>} style={{ width: 300 }} actions={[<EllipsisOutlined  key = "tab2"/>]} hoverable = "true" >   
+            <Card title= {'Flight Number: ' + this.props.flight_id} extra={<a><HeartOutlined /></a>} style={{ width: 300 }} hoverable = "true" >   
                 <p>Departure Time: {this.props.depature_time} </p>
                 <p>Arrival Time: {this.props.arival_time} </p>
-                <p>Trip Duration: {this.props.trip_duration} </p>
-                <p>Cabin Class: {this.props.cabin_class} </p>
-                <p>Baggage Allowance: {this.props.baggage} </p>
                 <p>Price: {this.props.price} </p>
+            <Menu
+            mode="inline"
+            style={{ height: '100%' , width: '100%' }}
+            >
+            <SubMenu>
+                <p style={{height: 20}}>Trip Duration: {this.props.trip_duration} </p>
+                <p style={{height: 20}}>Cabin Class: {this.props.cabin_class} </p>
+                <p style={{height: 20}}>Baggage Allowance: {this.props.baggage} </p>
+            </SubMenu>
+            </Menu>
             </Card>
             </div>
+            
 
 );
         }
